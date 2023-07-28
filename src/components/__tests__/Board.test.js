16af8b836de050fbfe82ef98714dbfaf0a7af5a3
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-28 14:49:15 -0400

    test: render & functionality
    -Test if all elements on board render as intended. -Test if clicking squares on the board behave as intended.

--- src/components/__tests__/Board.test.js
+++ src/components/__tests__/Board.test.js
@@ -1,6 +1,7 @@
 import { screen, render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Board from "../Board";
+import Styles from "./Board.module.scss";
 
 describe("Check renders and display", () => {
   it("should render a board with 9 squares", () => {
@@ -12,21 +13,35 @@ describe("Check renders and display", () => {
     expect(divElement).toBeInTheDocument();
   });
 
-  // I could not get this test to work. Apparently it has to do with the way the test doesn't render CSS from styled components.
-  //   it("should not render x or o element when board first loads", () => {
-  //     render(<Board squares={Array(9).fill("X")} />);
-  //     const spanElements = screen.queryAllByTestId(/charx/i);
-  //     spanElements.forEach((element) => expect(element).not.toBeVisible());
-  //   });
+  it("should not render x or o element when board first loads", () => {
+    render(<Board squares={Array(9).fill("X")} />);
+    const spanElements = screen.getAllByTestId(/charx/i);
+    spanElements.forEach((element) =>
+      expect(element).toHaveClass(Styles.square__text)
+    );
+  });
 });
 
-// Won't be able to get this test to work until I figure out how to detect visibility changes from styled components.
-// describe("Check functionality", () => {
-//   it("should display x or o when square is clicked", () => {
-//     render(<Board squares={Array(9).fill("")} />);
-//     const inputElements = screen.getAllByRole("checkbox");
-//     fireEvent(inputElements).click();
-//     const spanElements = expect(inputElements).toHaveLength(9);
-//     expect(divElement).toBeInTheDocument();
-//   });
-// });
+describe("Check functionality", () => {
+  function toggleCheckbox(inputElements) {
+    inputElements.forEach((element) =>
+      fireEvent.change(element, { target: { checked: true } })
+    );
+    return inputElements.forEach((element) =>
+      expect(element.checked).toBe(true)
+    );
+  }
+
+  it("should activate checkbox when clicked", () => {
+    render(<Board squares={Array(9).fill("X")} />);
+    const inputElements = screen.getAllByRole("checkbox");
+    toggleCheckbox(inputElements);
+  });
+  
+  it("should not toggle checkbox to false/unchecked on subsequent clicks", () => {
+    render(<Board squares={Array(9).fill("X")} />);
+    const inputElements = screen.getAllByRole("checkbox");
+    toggleCheckbox(inputElements);
+    toggleCheckbox(inputElements);
+  });
+});
